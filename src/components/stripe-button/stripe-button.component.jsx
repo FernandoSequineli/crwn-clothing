c4b461b69b12a85fab623f74b99b6203b4a6a2a4
Fernando Sequineli2 <fernandosequineli2@Fernandos-MacBook-Pro.local>
2021-07-08 09:23:15 +0100

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JArqqI1RvlHGdHvZ2pkel8hP16mlFx3sygNKJAF9DdIs1d5lSrlPZVKPkkUclC2zz2gfiPY5ZAy2w9QkoyvZZvA00T81C7FGG";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
